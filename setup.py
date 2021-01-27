ab931e86d8b2a0205f2ac9822f738c012f112a26
Sebastian Macaluso <sm4511@nyu.edu>
2021-01-27 15:44:21 -0300

    version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="ClusterTrellis",
-    version="0.0.2",
+    version="0.0.3",
     description="Hierarchical Cluster Trellis",
     url="https://github.com/SebastianMacaluso/ClusterTrellis",
     author="",
